be588810d6c9cdbe3180a0953733f2376e96de3e
malvika02 <malvika2ndmar2000@gmail.com>
2024-03-14 12:27:58 +0530

    created a basic home page with a nav

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,24 @@
+import Nav from '../components/nav'
+
 const Home = () => {
+    const authToken = true
+
+    const handleClick = () => {
+        console.log('clicked')
+    }
     return (
-        <div>Home</div>
+        // wrap in two empty divs because jsx needs to be wrapped
+        <> 
+        <Nav/>
+        <div  className = "home" >
+            <h1> Swipe Right </h1>
+            <button className="primary-button" onClick={handleClick}>
+                {authToken ? 'Signout' : 'Create Account'}
+            </button>
+
+
+        </div>
+        </>
     )
 }
 export default Home
